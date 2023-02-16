af2929b6db4936f71019b781149504c419647af5
abing <410174833@qq.com>
2023-02-16 18:47:59 +0800

    update

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,40 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-/// @custom:security-contact ltmuyuan@163.com
-contract ERC20 is Initializable, ERC20Upgradeable, PausableUpgradeable, AccessControlUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable {
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+contract MyToken is ERC20, ERC20Burnable, Pausable, Ownable {
+    constructor() ERC20("MyToken", "MTK") {}
 
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() public {
-        _disableInitializers();
-    }
-
-    function initialize() public initializer {
-        __ERC20_init("MyToken", "MTK");
-        __Pausable_init();
-        __AccessControl_init();
-        __ERC20Permit_init("MyToken");
-        __ERC20Votes_init();
-
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
-        _mint(msg.sender, 18 * 10 ** decimals());
-    }
-
-    function pause() public onlyRole(PAUSER_ROLE) {
+    function pause() public onlyOwner {
         _pause();
     }
 
-    function unpause() public onlyRole(PAUSER_ROLE) {
+    function unpause() public onlyOwner {
         _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount)
@@ -43,28 +27,5 @@ contract ERC20 is Initializable, ERC20Upgradeable, PausableUpgradeable, AccessCo
     override
     {
         super._beforeTokenTransfer(from, to, amount);
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _afterTokenTransfer(address from, address to, uint256 amount)
-    internal
-    override(ERC20Upgradeable, ERC20VotesUpgradeable)
-    {
-        super._afterTokenTransfer(from, to, amount);
-    }
-
-    function _mint(address to, uint256 amount)
-    internal
-    override(ERC20Upgradeable, ERC20VotesUpgradeable)
-    {
-        super._mint(to, amount);
-    }
-
-    function _burn(address account, uint256 amount)
-    internal
-    override(ERC20Upgradeable, ERC20VotesUpgradeable)
-    {
-        super._burn(account, amount);
     }
 }
